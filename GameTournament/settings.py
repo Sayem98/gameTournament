bfef01ecfa0060ca08354e43bba65624d8737900
Sayem98 <sayemabedin.bd@gmail.com>
2021-07-12 16:07:09 +0600

    Profile app created

--- GameTournament/settings.py
+++ GameTournament/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'login_site',
+    'profile_site',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ profile_site/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProfileSiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'profile_site'
