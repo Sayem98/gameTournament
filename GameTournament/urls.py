2b63de5d99671b855c7069bb6c5b5bc72d2d38a5
Sayem98 <sayemabedin.bd@gmail.com>
2021-07-12 16:10:32 +0600

    urls are updated

--- GameTournament/urls.py
+++ GameTournament/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('login_site.urls'))
+    path('', include('login_site.urls')),
+    path('/profile', include('profile_site.urls'))
 ]
