f33c06e07a7cd3e92ba26d874e489a125d6f1973
Sayem98 <sayemabedin.bd@gmail.com>
2021-07-12 21:26:25 +0600

    some chenges

--- profile_site/models.py
+++ profile_site/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from login_site.models import UserModel
+
 
 # Create your models here.
+
+class ProfileModel(models.Model):
+    user = models.OneToOneField(UserModel, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
