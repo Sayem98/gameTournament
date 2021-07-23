00325fbb12452f14711ec92b9f083ddd91e0342b
sayem98 <sayemabedin.bd@gmail.com>
2021-07-23 12:46:26 +0600

    allowedhost is added

--- GameTournament/settings.py
+++ GameTournament/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j=b1c&i@l@z2^@gu6o9_8s9&*!wrmpz$dm(_tvb4#8zcqvr9+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['play-tournament.herokuapp.com']
 
 # Application definition
 
